2f954e2cb7035113e5df7675731bf9c13687f520
Nycollas Kaique <nycollaskaique@hotmail.com>
2024-06-11 15:41:48 -0300

    :sparkles: aula de node js rocketseat

---
+++ node-do-zero/create-table.js
@@ -0,0 +1,15 @@
+import sql from "./db.js";
+
+// sql`DROP TABLE IF EXISTS video;`.then(() => {
+//   console.log("tabela apagada!");
+// });
+sql`
+  CREATE TABLE videos (
+    id TEXT PRIMARY KEY,
+    title TEXT,
+    description TEXT,
+    duration INTEGER
+  );
+`.then(() => {
+  console.log("tabela criada!");
+});
